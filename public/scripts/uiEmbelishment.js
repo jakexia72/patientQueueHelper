8254a46fe19650150e92527b7026f0f4b88877d5
jakexia72 <jakexia72@hotmail.com>
2019-08-21 23:00:41 -0400

    first working protoype

--- public/scripts/uiEmbelishment.js
+++ public/scripts/uiEmbelishment.js
@@ -10,3 +10,28 @@ function getOrdinal(num){
     return num + "th";
   }
 }
+
+function getNiceTimeFromMinutes(min){
+  if(min < 60){
+    return min + " minute" + getPluralSuffix(min);
+  }else if(min == 60){
+    return "one hour"
+  }else if(min > 60){
+    console.log(min)
+    hours = Math.floor(min/60);
+    remainingMin = min - (hours*60);
+    if (remainingMin == 0){
+      return hours + " hour" + getPluralSuffix(hours);
+    } else {
+      return (hours + " hour" + getPluralSuffix(hours) + ", " + remainingMin + " minute" + getPluralSuffix(remainingMin))
+    }
+  }
+}
+
+function getPluralSuffix(amount){
+    if (amount != 1){
+      return "s";
+    } else {
+      return "";
+    }
+}
